3d3cba149216a8684f281e69cb75a631692ad6a9
Anmol Singh <119476838+Anmol-26@users.noreply.github.com>
2023-10-09 20:37:10 +0530

    Add files via upload

---
+++ Java_Script/Insertion_sort.js
@@ -0,0 +1,58 @@
+/*
+   
+    Done by: Anmol Singh
+
+*/
+function Insertion()
+{
+    document.getElementById("worst").innerText="O(N^2)";
+    document.getElementById("average").innerText="O(N^2)";
+    document.getElementById("best").innerText="O(N)";
+
+    document.getElementById("space_worst").innerText="O(1)";
+
+    c_delay=0;
+
+    for(var j=0;j<array_size;j++)
+    {
+        div_update(divs[j],div_sizes[j],"yellow");//Color update
+
+        var key= div_sizes[j];
+        var i=j-1;
+        while(i>=0 && div_sizes[i]>key)
+        {
+            div_update(divs[i],div_sizes[i],"red");
+            div_update(divs[i+1],div_sizes[i+1],"red");
+
+            div_sizes[i+1]=div_sizes[i];
+
+            div_update(divs[i],div_sizes[i],"red");
+            div_update(divs[i+1],div_sizes[i+1],"red");
+    
+            div_update(divs[i],div_sizes[i],"blue");
+            if(i==(j-1))
+            {
+                div_update(divs[i+1],div_sizes[i+1],"yellow");
+            }
+            else
+            {
+                div_update(divs[i+1],div_sizes[i+1],"blue");
+            }
+            i-=1;
+        }
+        div_sizes[i+1]=key;
+
+        for(var t=0;t<j;t++)
+        {
+            div_update(divs[t],div_sizes[t],"green");
+        }
+    }
+    div_update(divs[j-1],div_sizes[j-1],"green");
+
+    enable_buttons();
+}
+/*
+   
+    Done by: Anmol Singh
+
+*/
---
+++ Java_Script/Selection_sort.js
@@ -0,0 +1,61 @@
+/*
+   
+    Done by: Anmol Singh
+
+*/
+function Selection_sort()
+{
+    document.getElementById("worst").innerText="O(N^2)";
+    document.getElementById("average").innerText="O(N^2)";
+    document.getElementById("best").innerText="O(N^2)";
+
+    document.getElementById("space_worst").innerText="O(1)";
+
+    c_delay=0;
+
+    for(var i=0;i<array_size-1;i++)
+    {
+        div_update(divs[i],div_sizes[i],"red");
+
+        index_min=i;
+
+        for(var j=i+1;j<array_size;j++)
+        {
+            div_update(divs[j],div_sizes[j],"yellow");
+
+            if(div_sizes[j]<div_sizes[index_min])
+            {
+                if(index_min!=i)
+                {
+                    div_update(divs[index_min],div_sizes[index_min],"blue");
+                }
+                index_min=j;
+                div_update(divs[index_min],div_sizes[index_min],"red");
+            }
+            else
+            {
+                div_update(divs[j],div_sizes[j],"blue");
+            }
+        }
+        
+        if(index_min!=i)
+        {
+            var temp=div_sizes[index_min];
+            div_sizes[index_min]=div_sizes[i];
+            div_sizes[i]=temp;
+
+            div_update(divs[index_min],div_sizes[index_min],"red");
+            div_update(divs[i],div_sizes[i],"red");
+            div_update(divs[index_min],div_sizes[index_min],"blue");
+        }
+        div_update(divs[i],div_sizes[i],"green");
+    }
+    div_update(divs[i],div_sizes[i],"green");
+
+    enable_buttons();
+}
+/*
+   
+    Done by: Anmol Singh
+
+*/
